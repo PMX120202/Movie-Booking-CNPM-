580460656f0b24ef3662d5d502d4fa30894f3386
antrvan746 <86640630+antrvan746@users.noreply.github.com>
2022-12-16 22:34:34 +0700

    Change some minor things

--- src/client/src/pages/login/Login.jsx
+++ src/client/src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import {AuthContext} from "../../context/AuthContext"
+import { AuthContext } from "../../context/AuthContext"
 import { useState,useContext } from "react"
 import { Link,useNavigate } from "react-router-dom"
 import "./Login.css"
